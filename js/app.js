b2844ca05b451758638f7d50f54c10e4876eb7b7
Scott Barclay <scottbarclay.js@gmail.com>
2020-07-30 14:22:03 -0700

    coming soon page

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const comingSoonBG = document.querySelectorAll('.bgImage');
+console.log(comingSoonBG[0]);
+let current = 0;
+
+bgTransition();
+function bgTransition() {
+	for (let i = 0; i < comingSoonBG.length; i++) {
+		comingSoonBG[i].classList.remove('showImg');
+		comingSoonBG[i].classList.add('hideImg');
+	}
+	comingSoonBG[current].classList.remove('hideImg');
+	comingSoonBG[current].classList.add('showImg');
+
+	current < comingSoonBG.length - 1 ? current++ : (current = 0);
+}
+
+let t = setInterval(bgTransition, 7000);
+
+const getInTouch = document.querySelector('button');
+const contact = document.querySelector('.contact');
+const back = document.querySelector('.back');
+const leftScreen1 = document.querySelector('.left-screen1');
+const leftScreen2 = document.querySelector('.left-screen2');
+
+getInTouch.addEventListener('click', () => {
+	leftScreen1.classList.remove('moveDown');
+	leftScreen2.classList.remove('moveDown');
+	leftScreen1.classList.add('moveUp');
+	leftScreen2.classList.add('moveUp');
+});
+
+back.addEventListener('click', () => {
+	leftScreen1.classList.remove('moveUp');
+	leftScreen2.classList.remove('moveUp');
+	leftScreen1.classList.add('moveDown');
+	leftScreen2.classList.add('moveDown');
+});
